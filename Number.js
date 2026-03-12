cd0dab2ab335fc91bab24a3a10cf2c320d902098
luisfelipevidal <luis.felipe.vidal@escola.pr.gov.br>
2026-03-12 08:32:00 -0300

    Add files via upload

---
+++ Number.js
@@ -0,0 +1,12 @@
+const meuNumero = 3
+
+const primeiroNumero = 1
+const segundoNumero = 2
+
+const operacaoMatematica = primeiroNumero + segundoNumero
+console.log(operacaoMatematica)
+
+const numemoPontoFlutuante = 3.3
+const pontoFlutuante = .5
+const novaOperacao = primeiroNumero / pontoFlutuante
+console.log(novaOperacao)
---
+++ string.js
@@ -0,0 +1,9 @@
+//Exemplos de texto
+let nome = "Luis"
+let sobrenome = "Vidal"
+let nomeCompleto = nome + " " + sobrenome
+//Concatenação das variáveis (+)
+console.log(nomeCompleto)
+let senha = "Senha1234"
+let stringDeNumeros = "123456789"
+console.log(stringDeNumeros + 2)
